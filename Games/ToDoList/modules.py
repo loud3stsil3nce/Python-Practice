4720597d8f2bfd008fcf5471b1dae8029ee79769
loud3stsil3nce <rafiurrahman325@gmail.com>
2025-03-06 22:23:45 -0500

    Finally, I have added an SQL Database to this project, all new tasks and task descriptions are added to a table. Currently have: Add task to db, print out tasks from db. In progress: Remove from db.

--- Games/ToDoList/modules.py
+++ Games/ToDoList/modules.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import db
 
 taskname = ''
 description = ''
@@ -6,13 +7,18 @@ ToDo = []
 task = {}
 num = len(ToDo)
 
+cursor = db.mycursor
+database = db.db
+
 
 
 def menu():
     try:
-        action = int(input("What would you like to do?\n1)View tasks\n2)Add Tasks\n3)Remove Tasks:\n"))
+        action = int(input("What would you like to do?\n1)View tasks\n2)Add Tasks\n3)Remove Tasks: "))
+        print("")
     except ValueError:
-        action = int(input("Please type in a valid number:\n1)View tasks\n2)Add Tasks\n3)Remove Tasks:\n"))
+        action = int(input("Please type in a valid number:\n1)View tasks\n2)Add Tasks\n3)Remove Tasks: "))
+        print("")
     return action
 def asktask():
     global taskname
@@ -33,15 +39,25 @@ def addtask(name, description):
     with open(todofile, "a") as todofile:    
         todofile.write(f"{len(ToDo)}. {name}: \n{description}\n")
     todofile.close()
+    cursor.execute("INSERT INTO ToDo (Item, Description, Status) VALUES (%s,%s,%s)", (name, description, "IP"))
+    database.commit()
 
 def viewalltasks():
+    cursor.execute("SELECT * FROM ToDo")
+    rows = cursor.fetchall()
+    num = 1
+    for row in rows:
+        print(f"{num}. {row[0]}\n{row[1]}\nStatus: {row[2]}\n")
+        num += 1
+        
+        
     
-    if len(ToDo) == 0:
-        print("No tasks found")
-    else:
-        for task in ToDo:
-            for key, value in task.items():
-                print(f"{ToDo.index(task) + 1}. {key}:\n{value}\n")
+    #if len(ToDo) == 0:
+        #print("No tasks found")
+    #else:
+        #for task in ToDo:
+            #for key, value in task.items():
+                #print(f"{ToDo.index(task) + 1}. {key}:\n{value}\n")
      
        
 
